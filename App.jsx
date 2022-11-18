59702cc86e4352626b94363e540a79f41c977115
loic <loic.rouzaud@epitech.eu>
2022-11-18 18:33:41 +0100

    [ADD] edit me

--- App.jsx
+++ App.jsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import Monalisa from "./assets/Monalisa.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.myDiv}>
+        <Text style={{ fontSize: 50, color: "white" }}>Edit Me !!!!!</Text>
+      </View>
+      <View>
+        <Image source={Monalisa} style={{ width: 300, height: 300 }} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  myDiv: {
+    alignItems: "center",
+    backgroundColor: "blue",
+    width: "80%",
+    borderRadius: 10,
+    marginBottom: 30,
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# TODO-ReactNative
+# TODO React Native
+
+Durant ce workshop vous allez voir les rudiments du React Native.
+Ce sera surement difficile au debut mais ne vous inquiétez pas, c'est génial. 
+
+Vous allez devoir reproduire une TODO-list.
+
+Vous avez à votre disposition des fichiers, créés automatiquement 
+par une commande :   
+
+```bash
+  npx create-expo-app AwesomeProject
+```
+
+## Documentation
+
+Voici votre Bible :
+
+ - [Documentation React Native](https://reactnative.dev/docs/getting-started)
